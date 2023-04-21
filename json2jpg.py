384032f6ac372032b459c65b6aecbcf503e460a8
Kwangsu Ahn <44615969+jubileekr@users.noreply.github.com>
2023-04-21 14:17:20 +0900

    Update json2jpg.py

--- json2jpg.py
+++ json2jpg.py
@@ -17,10 +17,11 @@ category_colors = {
 # Create blended image
 for image in data['images']:
     image_id = image['id']
-    image_path = os.path.join('images', image['file_name'])
-    img = Image.open(image_path)
-    img = img.convert('RGB')
-    mask = Image.new('RGB', img.size, color=(0, 0, 0))
+    #image_pathrgb = os.path.join('images', image['file_name'])
+    image_pathL = os.path.join('images', image['file_name'])
+    img = Image.open(image_pathrgb)
+    maskrgb = Image.new('RGB', img.size, color=(0, 0, 0))
+    maskL = Image.new('L', img.size)
 
     # Draw polygons for all categories in the image
     for annotation in data['annotations']:
@@ -29,17 +30,23 @@ for image in data['images']:
             color = category_colors.get(category_id)
             segmentation = annotation['segmentation'][0]
             polygon = [(segmentation[i], segmentation[i+1]) for i in range(0, len(segmentation), 2)]
-            draw = ImageDraw.Draw(mask)
-            draw.polygon(polygon, outline=color, fill=color)
+            drawrgb = ImageDraw.Draw(maskrgb)
+            drawL = ImageDraw.Draw(maskL)
+            drawrgb.polygon(polygon, outline=color, fill=color)
+            drawL.polygon(polygon, outline=category_id, fill=category_id)
 
-    # Blend original image with mask image
-    blended = Image.blend(img, mask, alpha=0.1)
+    # Blend original image with maskd image
+    #blended = Image.blend(img, maskrgb, alpha=0.1)
 
     # Save blended image with original filename in a different directory
-    image_name = os.path.basename(image_path)
-    save_path = os.path.join('blended', image_name)
-    blended.save(save_path)
+    #image_namergb = os.path.basename(image_pathrgb)
+    image_nameL = os.path.basename(image_pathL)
+    #save_pathrgb = os.path.join('blended', image_namergb)
+    save_pathL = os.path.join('blended', image_nameL)
+    #blended.save(save_pathrgb)
+    maskL.save(save_pathL)
+    
     print(f"Created {save_path}")
 
     save_path2 = os.path.join('labels', image_name)
-    mask.save(save_path2)
+    maskd.save(save_path2)
